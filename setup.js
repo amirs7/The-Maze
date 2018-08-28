fbdaa6ae793a85c6344466298d115c9ceac00206
amir <ahsprim@gmail.com>
2018-08-28 22:27:06 +0430

    add setup.js

---
+++ setup.js
@@ -0,0 +1,15 @@
+const User = require('./data_access/models/user');
+let user = new User({
+  _id: process.env.ADMIN_USERNAME,
+  name: process.env.ADMIN_NAME,
+  password: process.env.ADMIN_PASS,
+  role: 'admin'
+});
+
+const Maze = require('./data_access/models/maze');
+let maze = new Maze();
+maze.save().then(() => {
+  user.save().then(() => {
+    console.log('done');
+  });
+});
