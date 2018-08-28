83e9e9cf7e9f90514b31b4365935c429ec782b1b
amir <ahsprim@gmail.com>
2018-08-28 22:34:40 +0430

    fix await problem in setup

--- setup.js
+++ setup.js
@@ -1,18 +1,20 @@
 const User = require('./data_access/models/user');
-let user = new User({
-  _id: process.env.ADMIN_USERNAME,
-  name: process.env.ADMIN_NAME,
-  password: process.env.ADMIN_PASS,
-  role: 'admin'
-});
-
 const Maze = require('./data_access/models/maze');
-let maze;
-maze = Maze.findOne({});
-if(!maze)
-  maze = new Maze();
-maze.save().then(() => {
-  user.save().then(() => {
-    console.log('done');
+
+async function setup(){
+  let user = new User({
+    _id: process.env.ADMIN_USERNAME,
+    name: process.env.ADMIN_NAME,
+    password: process.env.ADMIN_PASS,
+    role: 'admin'
   });
-});
+  let maze;
+  maze = await Maze.findOne({});
+  if(!maze)
+    maze = new Maze();
+  maze.save().then(() => {
+    user.save().then(() => {
+      console.log('done');
+    });
+  });
+};
